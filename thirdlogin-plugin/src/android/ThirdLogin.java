89e4a17c7ebfbc112364dfbc64ecc1e1e0509923
z <zhuhaiwen@microduino.cc>
2018-11-14 16:40:09 +0800

    a

--- thirdlogin-plugin/src/android/ThirdLogin.java
+++ thirdlogin-plugin/src/android/ThirdLogin.java
@@ -1,23 +1,21 @@
-package com.z.thirdlogin;
+package com.microduino.mDesigner;
 
-/*
- * ：Created by z on 2018/11/14
- */
-
+import org.apache.cordova.CordovaPlugin;
 import android.widget.Toast;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONException;
-
+/*
+ * ：Created by z on 2018/11/14
+ */
 public class ThirdLogin extends CordovaPlugin {
-
     @Override
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
-        if ("show".equals(action)) {
+        if ("show".equals(action)){
             // 获取activity和context --> cordova.getActivity()和cordova.getContext()
-            Toast.makeText(cordova.getContext(), args.getString(0), Toast.LENGTH_SHORT).show();
+            Toast.makeText(cordova.getContext(),args.getString(0),Toast.LENGTH_SHORT).show();
             return true;
         }
         return false;
---
+++ thirdlogin-plugin/www/thirdlogin-plugin.js
@@ -0,0 +1,9 @@
+
+var exec = require('cordova/exec');
+
+// ToastShow： 是plugin.xml文件中的feature标签 name属性
+// show：是js中调用的方法名
+// [arg0]: 表示一个参数，[arg0,arg1]:表示两个参数
+exports.show = function (arg0, success, error) {
+    exec(success, error, 'ThirdLogin', 'show', [arg0]);
+};
