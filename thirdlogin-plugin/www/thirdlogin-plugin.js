069531a01434cc60038696ee245cd10a8cd52056
z <zhuhaiwen@microduino.cc>
2018-11-14 17:28:55 +0800

    sharSDK

--- thirdlogin-plugin/www/thirdlogin-plugin.js
+++ thirdlogin-plugin/www/thirdlogin-plugin.js
@@ -4,6 +4,6 @@ var exec = require('cordova/exec');
 // ToastShow： 是plugin.xml文件中的feature标签 name属性
 // show：是js中调用的方法名
 // [arg0]: 表示一个参数，[arg0,arg1]:表示两个参数
-exports.show = function (arg0, success, error) {
-    exec(success, error, 'ThirdLogin', 'show', [arg0]);
+exports.doLogin = function (arg0, success, error) {
+    exec(success, error, 'ThirdLogin', 'doLogin', [arg0]);
 };
