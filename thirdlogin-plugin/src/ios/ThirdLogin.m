0e61d09f00e873165ef118755840e7e7e7fd0548
z <zhuhaiwen@microduino.cc>
2018-11-19 19:01:23 +0800

    a

--- thirdlogin-plugin/src/ios/ThirdLogin.m
+++ thirdlogin-plugin/src/ios/ThirdLogin.m
@@ -57,18 +57,20 @@
                  NSLog(@"%@", user.url);
                  NSLog(@"***************************************************");
                  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                                  messageAsDictionary:user.rawData];
+                                                               messageAsDictionary:[self convertToJsonData:[user rawData]]];
                  [self.commandDelegate sendPluginResult:pluginResult callbackId:args.callbackId];
              }else{
                  NSLog(@"%@",error);
+                 NSString *err = [error localizedDescription];
+                 if(state == SSDKResponseStateCancel){
+                     err = @"0";
+                 }
                  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                                 messageAsString:[NSString stringWithFormat:@"error"]];
+                                                 messageAsString:[NSString stringWithFormat:err]];
                  [self.commandDelegate sendPluginResult:pluginResult callbackId:args.callbackId];
              }
          }];
     }
-
-    [self.commandDelegate sendPluginResult:nil callbackId:args.callbackId];
 }
 -(void)registSDKAppkey{
     [MobSDK registerAppKey:@"28c038beb20d6" appSecret:@"e1d6dd766e21ac7fbdd25b91efd5f712"];
@@ -100,4 +102,27 @@
     [alert addAction:centain];
     [[self viewController] presentViewController:alert animated:true completion:nil];
 }
+-(NSString *)convertToJsonData:(NSDictionary *)dict{
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString;
+    if (!jsonData) {
+        NSLog(@"%@",error);
+    }else{
+        jsonString = [[NSString alloc]initWithData:jsonData encoding:NSUTF8StringEncoding];
+    }
+    NSMutableString *mutStr = [NSMutableString stringWithString:jsonString];
+    NSRange range = {0,jsonString.length};
+    //去掉字符串中的空格
+    [mutStr replaceOccurrencesOfString:@" " withString:@"" options:NSLiteralSearch range:range];
+    NSRange range2 = {0,mutStr.length};
+    //去掉字符串中的换行符
+    [mutStr replaceOccurrencesOfString:@"\n" withString:@"" options:NSLiteralSearch range:range2];
+    NSRange range3 = {0,mutStr.length};
+    //去掉字符串中的斜杠
+    [mutStr replaceOccurrencesOfString:@"\\" withString:@""  options:NSLiteralSearch range:range3];
+    return mutStr;
+
+}
+
 @end
